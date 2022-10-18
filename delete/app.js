bf0f9410469f28b79b2871a97cc5d0b01214fa94
Mert <58515297+L0weN@users.noreply.github.com>
2022-10-18 18:54:17 +0300

    Add files via upload

---
+++ delete/app.js
@@ -0,0 +1,21 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+const middleware = function (req, res, next) {
+  console.log("Yeni bir istek geldi.");
+  next();
+};
+
+app.delete("/hello", middleware, function (req, res) {
+  res.json("Merhaba, DELETE isteği attınız");
+});
+
+app.listen(PORT, () => {
+  console.log("Ready on http://localhost:" + PORT);
+});
